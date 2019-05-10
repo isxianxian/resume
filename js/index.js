bea80e33bfcdfa33c8da9c11f579dd861df2526c
xianxian <15927621756@163.com>
2019-05-10 11:35:19 +0800

    last

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 $(function(){
+    $(document).ready(function () {
+
+        if(location.href.indexOf("#reloaded")==-1){
+           location.href=location.href+"#reloaded";
+           location.reload();
+       }
+    })
+
     let myAudio = $("#audio")[0];
     let $music = $('.mainBox>.music');
     setTimeout(function(){
         myAudio.play();
+        // myrefresh();
     })
     let i = 1;
     $music.on('click' , function(){
